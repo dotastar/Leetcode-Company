5ff76ec1c252f70031c1b7699b0129ff6a464cb2
Asia Cao's Macbook <cyz19892002@gmail.com>
2014-12-20 19:52:34 -0500

    finish CH6Q10~Q24

---
+++ MyJava/src/interview/epi/chaper6_array/Q12_Enumerate_All_Primes_To_n.java
@@ -0,0 +1,103 @@
+package interview.epi.chaper6_array;
+
+import static org.junit.Assert.assertTrue;
+import interview.AutoTestUtils;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+public class Q12_Enumerate_All_Primes_To_n {
+
+	static String methodName = "generatePrimesFrom1toN";
+	static Class<?> c = Q12_Enumerate_All_Primes_To_n.class;
+
+	public static void main(String[] args) {
+		AutoTestUtils.runTestClassAndPrint(c);
+	}
+
+	/**
+	 * Time: greater than O(n), but close to O(n)
+	 * Two optimizations:
+	 * 1.just need to try calculated primes (instead of all odd numbers)
+	 * 2.just need to try the number less than or equal to sqrt(n)
+	 */
+	public static List<Integer> generatePrimesFrom1toN(int n) {
+		List<Integer> primes = new ArrayList<Integer>();
+		if (n > 1)
+			primes.add(2);
+		int maxTry = (int) (Math.sqrt(n) + 1);
+		for (int i = 3; i < n; i += 2) {
+			boolean canDivid = false;
+			for (int j = 0; j < primes.size() && primes.get(j) < maxTry; j++) {
+				if (i % primes.get(j) == 0) {
+					canDivid = true;
+					break;
+				}
+			}
+			if (!canDivid)
+				primes.add(i);
+		}
+		return primes;
+	}
+
+	/**
+	 * Solution from EPI
+	 * Time: O(n/2 + n/3 + n/5 + n/7 + ...) = O(n * loglogn)
+	 */
+	public static List<Integer> generatePrimesFrom1toN_EPI(int n) {
+		int size = (int) Math.floor(0.5 * (n - 3)) + 1;
+		// Stores the primes from 1 to n.
+		List<Integer> primes = new ArrayList<>();
+		primes.add(2);
+		// isPrime[i] represents (2i + 3) is prime or not. Initially assuming
+		// everyone is prime (by setting as true).
+		boolean[] isPrime = new boolean[size];
+		Arrays.fill(isPrime, true);
+		for (int i = 0; i < size; ++i) {
+			if (isPrime[(int) i]) {
+				int p = (int) ((i * 2) + 3);
+				primes.add(p);
+				// Sieving from p^2, whose index is 4i^2 + 12i + 9 whose index
+				// in isPrime is 2i^2 + 6i + 3 because isPrime[i] represents 2i
+				// + 3.
+				for (long j = ((i * i) * 2) + 6 * i + 3; j < size; j += p) {
+					isPrime[(int) j] = false;
+				}
+			}
+		}
+		return primes;
+	}
+
+	/****************** Unit Test ******************/
+
+	@SuppressWarnings("unchecked")
+	public List<Integer> invokeMethod(Method m, int n) {
+		try {
+			Object res = m.invoke(null, n);
+			return (List<Integer>) res;
+		} catch (IllegalAccessException | IllegalArgumentException
+				| InvocationTargetException e) {
+			e.printStackTrace();
+			return new ArrayList<Integer>();
+		}
+	}
+
+	@Test
+	public void test1() {
+		for (Method m : AutoTestUtils.findMethod(methodName, c)) {
+			int n = 100;
+			List<Integer> res = invokeMethod(m, n);
+			int[] correct = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41,
+					43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 };
+			assertTrue(res.size() == correct.length);
+			for (int i = 0; i < correct.length; i++) {
+				assertTrue(res.get(i) == correct[i]);
+			}
+		}
+	}
+}
---
+++ MyJava/src/interview/epi/chaper6_array/Q20_Generate_Nonuniform_Random_Numbers.java
@@ -0,0 +1,175 @@
+package interview.epi.chaper6_array;
+
+import interview.AutoTestUtils;
+import interview.epi.Utils;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import org.junit.Test;
+
+/**
+ * Problem 6.20
+ * Given a set T of n distinct real numbers { t0, t1, t2, ..., tn-1 } and
+ * probabilities p0, p1, p2, ..., pn-1, which sum up to 1. Assume that
+ * t0 < t1 < ... < tn-1. Given a random number generator that produces values in
+ * [0,1] uniformly, how would you generate a number in T according to the
+ * specified probabilities?
+ * 
+ * @author yazhoucao
+ * 
+ */
+public class Q20_Generate_Nonuniform_Random_Numbers {
+	static String methodName = "nonuniformRandomNumberGeneration";
+	static Class<?> c = Q20_Generate_Nonuniform_Random_Numbers.class;
+
+	public static void main(String[] args) {
+		AutoTestUtils.runTestClassAndPrint(c);
+
+		/************* Test for bsearchRange() *************/
+		double[] arr2 = { 0.7537424049223119, 0.9722134043237963,
+				0.9882921426813501, 0.9926719295401996, 0.9967492130347728,
+				0.9983624218619668, 0.9999653545207491, 0.9999694983935403,
+				0.9999711427630388, 0.9999949148096311, 0.9999982869701197, 1.0 };
+		assert bsearchRange(arr2, 0.93d) == 1;
+
+		double[] arr1 = { 1, 3, 5, 9 };
+		assert bsearchRange(arr1, 0) == 0;
+		assert bsearchRange(arr1, 2) == 1;
+		assert bsearchRange(arr1, 10) == 3;
+
+		double[] arr = { 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1 };
+		assert bsearchRange(arr, 0.75) == 7;
+
+	}
+
+	/**
+	 * Time: O(n), n = length(T) = length(P).
+	 */
+	public static double nonuniformRandomNumberGeneration(List<Double> T,
+			List<Double> P) {
+		assert T.size() == P.size();
+		Random ran = new Random();
+		double choosen = ran.nextDouble();
+		double p = 0;
+		for (int i = 0; i < P.size(); i++) {
+			p += P.get(i);
+			if (choosen < p)
+				return T.get(i);
+		}
+		throw new RuntimeException("Probabilities of P don't sum to 1: " + p);
+	}
+
+	/**
+	 * Construct an array of cumulative probability in precomputation step.
+	 * Binary search the randomly generated real number sits in which range.
+	 * 
+	 * Precompute: O(n) time, O(n) space.
+	 * 
+	 * Algorithm: O(lgn) time.
+	 */
+	public static double nonuniformRandomNumberGeneration_improved(
+			List<Double> T, List<Double> P) {
+		// precomputation
+		assert T.size() == P.size();
+		double[] arrP = generateCumulativeArray(P);
+
+		// binary search
+		Random ran = new Random();
+		double val = ran.nextDouble();
+		int idx = bsearchRange(arrP, val);
+		if (idx >= 0 && idx < T.size())
+			return T.get(idx);
+		else
+			return -1;
+	}
+
+	/**
+	 * Binary search a range, a range is [ a, b ).
+	 */
+	public static int bsearchRange(double[] A, double val) {
+		if (A.length == 0)
+			return -1;
+		if (A.length == 1)
+			return val < A[0] ? 0 : -1;
+		int l = 0;
+		int r = A.length - 1;
+		while (l <= r) {
+			int mid = l + (r - l) / 2;
+			if ((val < A[mid] && mid == 0) // leftmost
+					|| (val >= A[mid] && mid == A.length - 1) // rightmost
+					|| (val < A[mid] && val >= A[mid - 1])) {
+				return mid;
+			} else if (val >= A[mid]) {
+				l = mid + 1;
+			} else
+				r = mid - 1;
+
+		}
+		return -1;
+	}
+
+	public static double[] generateCumulativeArray(List<Double> P) {
+		double[] arr = new double[P.size()];
+		arr[0] = P.get(0);
+		for (int i = 1; i < arr.length; i++)
+			arr[i] = arr[i - 1] + P.get(i);
+		return arr;
+	}
+
+	/****************** Unit Test ******************/
+
+	public Double invokeMethod(Method m, List<Double> T, List<Double> P) {
+		try {
+			Object[] args = new Object[] { T, P };
+			Object res = m.invoke(null, args);
+			return (Double) res;
+		} catch (IllegalAccessException | IllegalArgumentException
+				| InvocationTargetException e) {
+			e.printStackTrace();
+			return -1d;
+		}
+	}
+
+	@Test
+	public void test1() {
+		Random gen = new Random();
+		int n = gen.nextInt(50) + 1;
+		List<Double> T = new ArrayList<>(n);
+		Utils.iota(T, n, 0);
+		List<Double> P = new ArrayList<>();
+		double fullProb = 1.0;
+		for (int i = 0; i < n - 1; ++i) {
+			double pi = gen.nextDouble() * fullProb;
+			P.add(pi);
+			fullProb -= pi;
+		}
+		P.add(fullProb);
+
+		Utils.simplePrint(T);
+		System.out.println();
+
+		Utils.simplePrint(P);
+		System.out.println();
+		for (Method m : AutoTestUtils.findMethod(methodName, c)) {
+			// System.out.println(nonuniformRandomNumberGeneration(T, P));
+			// Test. Perform the nonuniform random number generation for
+			// n * kTimes times and calculate the distribution of each bucket.
+			int kTimes = 100000;
+			int[] counts = new int[n];
+			for (int i = 0; i < n * kTimes; ++i) {
+				double t = invokeMethod(m, T, P);
+				++counts[(int) t];
+			}
+			for (int i = 0; i < n; ++i) {
+				System.out.println((double) (counts[i]) / (n * kTimes) + " "
+						+ P.get(i));
+				assert Math.abs(((double) counts[i]) / (n * kTimes) - P.get(i)) < 0.01;
+			}
+		}
+	}
+
+}
