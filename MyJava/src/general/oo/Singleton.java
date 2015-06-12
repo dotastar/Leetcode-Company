85e7f9f4292b8bac520bbbdfdb831b54fde821e0
Asia Cao's Macbook <cyz19892002@gmail.com>
2015-06-12 19:37:37 -0400

    revised Singleton

--- MyJava/src/general/oo/Singleton.java
+++ MyJava/src/general/oo/Singleton.java
@@ -21,10 +21,22 @@ public class Singleton {
 		 * Thread A entered getInstance(), begin to initialize instance of
 		 * ClassSingle, because of Java's memory model, reference 'instance' is
 		 * not null during 'new ClassSingle()', at the same time, thread B
-		 * called getInstance() and get the uninitialized instance, in which
-		 * case would cause problems.
-		 * 
+		 * called getInstance() and get the instance which has not finished
+		 * initializing yet, in which case would cause problems.
 		 */
+
+		/**
+		 * volatile solves one issue that is visibility issue . If you are
+		 * writing to one variable that is declared volatile then the value will
+		 * be visible to other thread immediately. As we all know we have
+		 * different level of cache in os L1, L2, L3 and if we write to a
+		 * variable in one thread it is not guaranteed to be visible to other,
+		 * so if we use volatile it writes to direct memory and is visible to
+		 * others. But volatile does not solve the issue of atomicity.
+		 * i.e. int a; a++; is not safe.
+		 * AS there are three machine instructions associated to it.
+		 */
+
 		private volatile static ClassSingle instance;
 
 		// prevent initialization
@@ -35,9 +47,9 @@ public class Singleton {
 		}
 
 		// double check
-		public ClassSingle getInstance() {
+		public static ClassSingle getInstance() {
 			if (instance == null) {
-				synchronized (this) {
+				synchronized (ClassSingle.class) {
 					if (instance == null)
 						instance = new ClassSingle();
 				}
