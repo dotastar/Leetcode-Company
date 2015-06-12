db0278adea4f7a37180c349f882c1885f576734f
Asia Cao's Macbook <cyz19892002@gmail.com>
2015-06-11 20:07:34 -0400

    add an abstract class of PostCrawler, add multithreading

--- MyJava/src/general/webcrawler/forum/MeetQunPostCrawler.java
+++ MyJava/src/general/webcrawler/forum/MeetQunPostCrawler.java
@@ -11,27 +11,17 @@ import java.util.Calendar;
 import org.jsoup.Connection.Response;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
-public class MeetQunPostCrawler {
+public class MeetQunPostCrawler extends PostCrawler {
 
 	public static void main(String[] args) {
 		MeetQunPostCrawler pc = new MeetQunPostCrawler();
 		pc.crawlPostsToFile(ROOT_URL);
-
 	}
 
-	private static final String[] KEY_WORDS = { "booking", "connectifier", "vertica" };
-	private static final int MAX_PAGE = 140;
-	private static final long WAIT_TIME = 200; // ms
-	private static final int TIMEOUT = 10_000; // ms
-
-	private static final String ROOT_URL = "http://www.meetqun.com/forum-36-#.html";
-	private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0";
-	private static final String HTML_BEGIN = "<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\"> <html> <head> <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"> <meta http-equiv=\"Content-Style-Type\" content=\"text/css\"> <base href=\"http://www.meetqun.com/\"> <link rel=\"stylesheet\" type=\"text/css\" href=\"data/cache/style_7_common.css?K6D\"> <link rel=\"stylesheet\" type=\"text/css\" href=\"data/cache/style_7_forum_forumdisplay.css?K6D\">   <script src=\"data/cache/common.js?K6D\" type=\"text/javascript\"></script>  <link href=\"template/week_steady/images//js/portal.css\" rel=\"stylesheet\" type=\"text/css\"> <link rel=\"stylesheet\" id=\"css_widthauto\" type=\"text/css\" href=\"data/cache/style_7_widthauto?K6D\">  <script src=\"data/cache/common.js?K6D\" type=\"text/javascript\"></script>  <script type=\"text/javascript\">HTMLNODE.className += ' widthauto'</script>  <script src=\"data/cache/forum.js?K6D\" type=\"text/javascript\"></script>  <script src=\"data/cache/portal.js?K6D\" type=\"text/javascript\"></script>  <title>过滤面经</title> </head> <body id=\"nv_forum\" class=\"pg_forumdisplay\" onkeydown=\"if(event.keyCode==27) return false;\">  <table>";
-	private static final String HTML_END = "</table> </body> </html>";
-	private static final String HTML_CHECKBOX = "<td> <input type=\"checkbox\" name=\"read\" value=\"false\"> </td>";
+	protected static final int MAX_PAGE = 141;
+	protected static final String ROOT_URL = "http://www.meetqun.com/forum-36-#.html";
+	protected static final String HTML_BEGIN = "<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\"> <html> <head> <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"> <meta http-equiv=\"Content-Style-Type\" content=\"text/css\"> <base href=\"http://www.meetqun.com/\"> <link rel=\"stylesheet\" type=\"text/css\" href=\"data/cache/style_7_common.css?K6D\"> <link rel=\"stylesheet\" type=\"text/css\" href=\"data/cache/style_7_forum_forumdisplay.css?K6D\">   <script src=\"data/cache/common.js?K6D\" type=\"text/javascript\"></script>  <link href=\"template/week_steady/images//js/portal.css\" rel=\"stylesheet\" type=\"text/css\"> <link rel=\"stylesheet\" id=\"css_widthauto\" type=\"text/css\" href=\"data/cache/style_7_widthauto?K6D\">  <script src=\"data/cache/common.js?K6D\" type=\"text/javascript\"></script>  <script type=\"text/javascript\">HTMLNODE.className += ' widthauto'</script>  <script src=\"data/cache/forum.js?K6D\" type=\"text/javascript\"></script>  <script src=\"data/cache/portal.js?K6D\" type=\"text/javascript\"></script>  <title>过滤面经</title> </head> <body id=\"nv_forum\" class=\"pg_forumdisplay\" onkeydown=\"if(event.keyCode==27) return false;\">  <table>";
 
 	public MeetQunPostCrawler() {
 
@@ -70,52 +60,15 @@ public class MeetQunPostCrawler {
 			System.err.println(e.getMessage());
 		} finally {
 			appendCloseTag(file);
-			System.out.println("Ended at page " + pageIdx);
+			System.out.println("MeetQunPostCrawler ended at page " + pageIdx);
 		}
-		System.out.println("End crawling!");
+		System.out.println("MeetQunPostCrawler end crawling!");
 	}
 
-	/**
-	 * Append posts that contains the keyword to the HTML file
-	 */
-	private int appendPosts(Document page, BufferedWriter wr, int postId,
-			String[] keywords) {
-		int postCnt = 0;
-		for (String keyword : keywords) {
-			Elements eles = page.select("tbody:contains(" + keyword + ")");
-			for (Element ele : eles) {
-				try {
-					Element tr = ele.child(0); // <tr> tag
-					// append <td> Id </td> and <td> check-box </td>
-					tr.append("<td >" + postId + "</td>");
-					tr.append(HTML_CHECKBOX);
-					wr.write(ele.toString());
-					postId++;
-				} catch (IOException e) {
-					System.err.println(e.getMessage());
-				}
-			}
-			postCnt += eles.size();
-		}
-
-		return postCnt;
+	@Override
+	public void run() {
+		MeetQunPostCrawler pc = new MeetQunPostCrawler();
+		pc.crawlPostsToFile(ROOT_URL);
 	}
 
-	private void printCurrentProgress(int page, int postId) {
-		System.out.println(String.format(
-				"Has crawled page %d, and collected %d posts totaly.", page, postId - 1));
-	}
-
-	/**
-	 * Append </table> </body> </html> to the file
-	 * 
-	 * @param file
-	 */
-	private void appendCloseTag(File file) {
-		try (BufferedWriter wr = new BufferedWriter(new FileWriter(file, true))) {
-			wr.write(HTML_END);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
 }
--- MyJava/src/general/webcrawler/forum/OnePointThreeAcresPostCrawler.java
+++ MyJava/src/general/webcrawler/forum/OnePointThreeAcresPostCrawler.java
@@ -8,30 +8,20 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
-import org.jsoup.Jsoup;
 import org.jsoup.Connection.Response;
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
-public class OnePointThreeAcresPostCrawler {
+public class OnePointThreeAcresPostCrawler extends PostCrawler {
 
 	public static void main(String[] args) {
 		OnePointThreeAcresPostCrawler crawler = new OnePointThreeAcresPostCrawler();
 		crawler.crawlPostsToFile(ROOT_URL);
 	}
 
-	private static final String[] KEY_WORDS = { "booking", "connectifier", "vertica" };
-	// { "zenefits" };
-	private static final int MAX_PAGE = 68;
-	private static final long WAIT_TIME = 200; // ms
-	private static final int TIMEOUT = 20_000; // ms
-
-	private static final String ROOT_URL = "http://www.1point3acres.com/bbs/forum-145-#.html";
-	private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0";
-	private static final String HTML_BEGIN = "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\"><html xmlns=\"http://www.w3.org/1999/xhtml\"><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"><title>一亩三分地面经</title><base href=\"http://www.1point3acres.com/bbs/\" /><link rel=\"stylesheet\" type=\"text/css\" href=\"data/cache/style_5_common.css?D6I\" /><link rel=\"stylesheet\" type=\"text/css\" href=\"data/cache/style_5_forum_forumdisplay.css?D6I\" /><script src=\"data/cache/common.js?D6I\" type=\"text/javascript\"></script><link rel=\"stylesheet\" id=\"css_widthauto\" type=\"text/css\" href=\"data/cache/style_5_widthauto.css?D6I\" /><script type=\"text/javascript\">HTMLNODE.className += ' widthauto'</script><script src=\"data/cache/forum.js?D6I\" type=\"text/javascript\"></script></head><body id=\"nv_forum\" class=\"pg_forumdisplay\" onkeydown=\"if(event.keyCode==27) return false;\"><table>";
-	private static final String HTML_END = "</table> </body> </html>";
-	private static final String HTML_CHECKBOX = "<td> <input type=\"checkbox\" name=\"read\" value=\"false\"> </td>";
+	protected static final int MAX_PAGE = 68;
+	protected static final String ROOT_URL = "http://www.1point3acres.com/bbs/forum-145-#.html";
+	protected static final String HTML_BEGIN = "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\"><html xmlns=\"http://www.w3.org/1999/xhtml\"><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"><title>一亩三分地面经</title><base href=\"http://www.1point3acres.com/bbs/\" /><link rel=\"stylesheet\" type=\"text/css\" href=\"data/cache/style_5_common.css?D6I\" /><link rel=\"stylesheet\" type=\"text/css\" href=\"data/cache/style_5_forum_forumdisplay.css?D6I\" /><script src=\"data/cache/common.js?D6I\" type=\"text/javascript\"></script><link rel=\"stylesheet\" id=\"css_widthauto\" type=\"text/css\" href=\"data/cache/style_5_widthauto.css?D6I\" /><script type=\"text/javascript\">HTMLNODE.className += ' widthauto'</script><script src=\"data/cache/forum.js?D6I\" type=\"text/javascript\"></script></head><body id=\"nv_forum\" class=\"pg_forumdisplay\" onkeydown=\"if(event.keyCode==27) return false;\"><table>";
 
 	public void crawlPostsToFile(String root) {
 		DateFormat dateFormat = new SimpleDateFormat("HH-mm-MM-dd-yyyy");
@@ -66,52 +56,15 @@ public class OnePointThreeAcresPostCrawler {
 			System.err.println(e.getMessage());
 		} finally {
 			appendCloseTag(file);
-			System.out.println("Ended at page " + pageIdx);
+			System.out.println("OnePointThreeAcresPostCrawler ended at page " + pageIdx);
 		}
-		System.out.println("End crawling!");
+		System.out.println("OnePointThreeAcresPostCrawler end crawling!");
 	}
 
-	/**
-	 * Append posts that contains the keyword to the HTML file
-	 */
-	private int appendPosts(Document page, BufferedWriter wr, int postId,
-			String[] keywords) {
-		int postCnt = 0;
-		for (String keyword : keywords) {
-			Elements eles = page.select("tbody:contains(" + keyword + ")");
-			for (Element ele : eles) {
-				try {
-					Element tr = ele.child(0); // <tr> tag
-					// append <td> Id </td> and <td> check-box </td>
-					tr.append("<td >" + postId + "</td>");
-					tr.append(HTML_CHECKBOX);
-					wr.write(ele.toString());
-					postId++;
-				} catch (IOException e) {
-					System.err.println(e.getMessage());
-				}
-			}
-			postCnt += eles.size();
-		}
-
-		return postCnt;
+	@Override
+	public void run() {
+		OnePointThreeAcresPostCrawler crawler = new OnePointThreeAcresPostCrawler();
+		crawler.crawlPostsToFile(ROOT_URL);
 	}
 
-	private void printCurrentProgress(int page, int postId) {
-		System.out.println(String.format(
-				"Has crawled page %d, and collected %d posts totaly.", page, postId - 1));
-	}
-
-	/**
-	 * Append </table> </body> </html> to the file
-	 * 
-	 * @param file
-	 */
-	private void appendCloseTag(File file) {
-		try (BufferedWriter wr = new BufferedWriter(new FileWriter(file, true))) {
-			wr.write(HTML_END);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
 }
