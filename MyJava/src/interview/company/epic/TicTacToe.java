baf91aaafe621a8fc9147f720fc159bfc45829b6
Asia Cao's Macbook <cyz19892002@gmail.com>
2015-06-13 16:32:52 -0400

    add a title to TicTacToe

--- MyJava/src/interview/company/epic/TicTacToe.java
+++ MyJava/src/interview/company/epic/TicTacToe.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import interview.AutoTestUtils;
 
 /**
+ * Tic Tac Toe
  * N*N matrix is given with input red or black.You can move horizontally,
  * vertically or diagonally. If 3 consecutive same color found, that color will
  * get 1 point. So if 4 red are vertically then point is 2. Find the winner.
