a09f36fba4232fd68da74c5b18f5b71a3d7f60d1
asia cao <asia@connectifier.com>
2015-08-31 22:00:45 -0700

    tweak Encodings.java a little bit

--- MyJava/src/general/misc/Encodings.java
+++ MyJava/src/general/misc/Encodings.java
@@ -11,21 +11,16 @@ public class Encodings {
 		long id = 1235;
 		String encoded = base64Encoding(id); // "AAAAAAAABNM=";
 
-
-
 		System.out.println(encoded);
 
-
-
 		// encode to binary as one number
-		System.out.println(Integer.toBinaryString(5678));
+		System.out.println(Integer.toBinaryString(5678));		
 
-		// encode to binary as several digits
-		System.out.println(Integer.toBinaryString(5) + Integer.toBinaryString(6)
-				+ Integer.toBinaryString(7) + Integer.toBinaryString(8));
+		//encode to binary as several digits
+		System.out.println(Integer.toBinaryString(5)+Integer.toBinaryString(6)+Integer.toBinaryString(7)+Integer.toBinaryString(8));
+		
+		//encode to String of UTF-8 encoding
 
-
-		// encode to String of UTF-8 encoding
 		System.out.println(Integer.toBinaryString(5678).getBytes("UTF-8").length);
 		for (Byte b : Integer.toBinaryString(5678).getBytes("UTF-8")) {
 			System.out.print(Integer.toBinaryString(b) + "");
