daf3f7d795d1f5c96925e432622be4d8de9cd4ee
asia cao <cyz19892002@gmail.com>
2016-01-15 22:28:54 -0800

    Format change

--- MyJava/src/main/java/general/webcrawler/forum/MeetQunPostCrawler.java
+++ MyJava/src/main/java/general/webcrawler/forum/MeetQunPostCrawler.java
@@ -19,7 +19,7 @@ public class MeetQunPostCrawler extends PostCrawler {
 		pc.crawlPostsToFile(ROOT_URL);
 	}
 
-	protected static final int MAX_PAGE = 141;
+	protected static final int MAX_PAGE = 200;
 	protected static final String ROOT_URL = "http://www.meetqun.com/forum-36-#.html";
 	protected static final String HTML_BEGIN = "<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\"> <html> <head> <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"> <meta http-equiv=\"Content-Style-Type\" content=\"text/css\"> <base href=\"http://www.meetqun.com/\"> <link rel=\"stylesheet\" type=\"text/css\" href=\"data/cache/style_7_common.css?K6D\"> <link rel=\"stylesheet\" type=\"text/css\" href=\"data/cache/style_7_forum_forumdisplay.css?K6D\">   <script src=\"data/cache/common.js?K6D\" type=\"text/javascript\"></script>  <link href=\"template/week_steady/images//js/portal.css\" rel=\"stylesheet\" type=\"text/css\"> <link rel=\"stylesheet\" id=\"css_widthauto\" type=\"text/css\" href=\"data/cache/style_7_widthauto?K6D\">  <script src=\"data/cache/common.js?K6D\" type=\"text/javascript\"></script>  <script type=\"text/javascript\">HTMLNODE.className += ' widthauto'</script>  <script src=\"data/cache/forum.js?K6D\" type=\"text/javascript\"></script>  <script src=\"data/cache/portal.js?K6D\" type=\"text/javascript\"></script>  <title>过滤面经</title> </head> <body id=\"nv_forum\" class=\"pg_forumdisplay\" onkeydown=\"if(event.keyCode==27) return false;\">  <table>";
 
@@ -29,8 +29,7 @@ public class MeetQunPostCrawler extends PostCrawler {
 
 	public void crawlPostsToFile(String root) {
 		DateFormat dateFormat = new SimpleDateFormat("HH-mm-MM-dd-yyyy");
-		String fileName = "/Users/yazhoucao/Downloads/ForumPosts/mq-"
-				+ dateFormat.format(Calendar.getInstance().getTime()) + ".html";
+		String fileName = PostCrawler.ROOT_DIRECTORY + "mq-" + dateFormat.format(Calendar.getInstance().getTime()) + ".html";
 		File file = new File(fileName);
 		int pageIdx = 1;
 		try (BufferedWriter wr = new BufferedWriter(new FileWriter(file))) {
